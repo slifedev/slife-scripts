f7cd5388a1f91d0eb2dd9ba491af5ac4b2c1441b
Vladyslav Zayats <alivestalker@gmail.com>
2024-07-22 12:59:12 +0300

    add variables to uppercase and color changes

--- build.sh
+++ build.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 # VARIABLES
-frappe_docker_path=
-apps_path=
-frappe_repo=
+FRAPPE_DOCKER_PATH=
+APPS_PATH=
+FRAPPE_REPO=
 frappe_branch=
-yaml_path=
-image_tag=
-project_name=
-site_name=
+YAML_PATH=
+IMAGE_TAG=
+PROJECT_NAME=
+SITE_NAME=
 
 # FUNCTIONS
 log() {
@@ -16,7 +16,7 @@ log() {
 }
 
 error_exit() {
-  echo -e "\033[0;33m$(date +'%Y-%m-%d %H:%M:%S') - ERROR $1\033[0m"
+  echo -e "\033[0;31m$(date +'%Y-%m-%d %H:%M:%S') - ERROR $1\033[0m"
   exit 1
 }
 
@@ -25,28 +25,28 @@ warn() {
 }
 
 # OPERATIONS
-log "cd $frappe_docker_path"
-cd $frappe_docker_path || error_exit "Failed to cd $frappe_docker_path"
+log "cd $FRAPPE_DOCKER_PATH"
+cd $FRAPPE_DOCKER_PATH || error_exit "Failed to cd $FRAPPE_DOCKER_PATH"
 
-log "Generating base64 string from $apps_path"
-APPS_JSON_BASE64=$(base64 -w 0 $apps_path) || error_exit "Failed to generate base64 string from $apps_path"
+log "Generating base64 string from $APPS_PATH"
+APPS_JSON_BASE64=$(base64 -w 0 $APPS_PATH) || error_exit "Failed to generate base64 string from $APPS_PATH"
 
 log "Building an image"
 docker build --no-cache\
-  --build-arg=FRAPPE_PATH=$frappe_repo \
+  --build-arg=FRAPPE_PATH=$FRAPPE_REPO \
   --build-arg=FRAPPE_BRANCH=$frappe_branch \
   --build-arg=PYTHON_VERSION=3.10.12 \
   --build-arg=NODE_VERSION=20.11.0 \
   --build-arg=APPS_JSON_BASE64=$APPS_JSON_BASE64 \
-  -t=$image_tag \
+  -t=$IMAGE_TAG \
   -f=images/custom/Containerfile . || error_exit "Failed to build an image"
 
-log "Stopping $project_name"
-docker compose -p $project_name down || error_exit "Failed to stop $project_name"
-log "Starting $project_name from new image"
-docker compose -p $project_name -f $yaml_path up -d || error_exit "Failed to start $project_name from new image"
-log "Migrating $site_name"
-docker compose -p $project_name exec backend bench --site $site_name migrate || error_exit "Failed to migrate $site_name"
+log "Stopping $PROJECT_NAME"
+docker compose -p $PROJECT_NAME down || error_exit "Failed to stop $PROJECT_NAME"
+log "Starting $PROJECT_NAME from new image"
+docker compose -p $PROJECT_NAME -f $YAML_PATH up -d || error_exit "Failed to start $PROJECT_NAME from new image"
+log "Migrating $SITE_NAME"
+docker compose -p $PROJECT_NAME exec backend bench --site $SITE_NAME migrate || error_exit "Failed to migrate $SITE_NAME"
 
 # CLEAR ALL THE DANGLING IMAGES
 # log "Clearing dangling images"
@@ -55,4 +55,4 @@ docker compose -p $project_name exec backend bench --site $site_name migrate || 
 log "Clearing docker builder cache"
 docker builder prune -f || warn "Failed to clear docker builder cache"
 
-printf "\033[0;32m$site_name has been built and deployed\033[0m\n"
+printf "\033[0;32m$SITE_NAME has been built and deployed\033[0m\n"
